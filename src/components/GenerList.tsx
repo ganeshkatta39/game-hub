4bbc5a0e018b28c99c0f8e515e071a4730a1547b
Katta Ganesh <ganeshkatta39@gmail.com>
2023-04-13 22:36:05 +0530

    created genre list with images

--- src/components/GenerList.tsx
+++ src/components/GenerList.tsx
@@ -1,13 +1,25 @@
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useGeners from "../hooks/useGeners";
+import getCroppedImageUrl from "../services/image-urls";
 
 const GenerList = () => {
   const { data } = useGeners();
   return (
-    <ul>
+    <List>
       {data.map((gener) => (
-        <li key={gener.id}>{gener.name}</li>
+        <ListItem key={gener.id} paddingY="5px">
+          {" "}
+          <HStack>
+            <Image
+              boxSize="32px"
+              borderRadius={8}
+              src={getCroppedImageUrl(gener.image_background)}
+            />
+            <Text fontSize="lg">{gener.name}</Text>
+          </HStack>{" "}
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 
--- src/hooks/useGeners.ts
+++ src/hooks/useGeners.ts
@@ -3,6 +3,7 @@ import useData from "./useData";
 export interface Gener {
   id: number;
   name: string;
+  image_background: string;
 }
 
 const useGeners = () => useData<Gener>("/genres");
